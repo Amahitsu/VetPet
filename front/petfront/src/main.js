504fde30575b5588576fb137df0b84c4ab61f9d1
AMANDA HIRATA MEDEIROS <amanda.medeiros1@alunos.sc.senac.br>
2024-05-06 20:31:10 -0300

    adicionando routes no front para base

--- front/petfront/src/main.js
+++ front/petfront/src/main.js
@@ -3,4 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory} from 'vue-router'
+import Login from './components/Login.vue'
+import PrincipalScreen from './components/PrincipalScreen.vue'
+
+const routes = [
+    {path: '/', component: PrincipalScreen},
+    {path: '/Login', component: Login},
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes, 
+})
+
+createApp(App).use(router).mount('#app')

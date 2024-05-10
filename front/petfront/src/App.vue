75c99216bea7379a39c2bb4c8cf1a00b98adbf86
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-05-10 20:11:36 -0300

    arrumando erro

--- front/petfront/src/App.vue
+++ front/petfront/src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-<<<<<<< HEAD
 
 </script>
 
 <template>
   
   <router-view></router-view>
-=======
-</script>
-
-<template>
->>>>>>> affe984aa42fc7d59ca42be2607100c78d3e7e23
-
-<router-view>
-</router-view>>
 
 </template>
 
--- front/petfront/src/main.js
+++ front/petfront/src/main.js
@@ -12,11 +12,11 @@ import Month from './components/screenAgenda/Month.vue'
 import Week from './components/screenAgenda/Week.vue'
 import RegisterActivity from './components/screensRegister/RegisterActivity.vue'
 import RegisterAgenda from './components/screensRegister/RegisterAgenda.vue'
+import RegisterClient from './components/screensRegister/RegisterClient.vue'
 import RegisterEmployee from './components/screensRegister/RegisterEmployee.vue'
 import RegisterPetClient from './components/screensRegister/RegisterPetClient.vue'
 import RegisterRace from './components/screensRegister/RegisterRace.vue'
 import RegisterService from './components/screensRegister/RegisterService.vue'
-import RegisterClient from './components/screensRegister/RegisterClient.vue'
 
 
 
@@ -31,13 +31,6 @@ const routes = [
     {path: '/agenda/week', component:Week},
     
     //Registros de cadastro
-<<<<<<< HEAD
-    {path: '/Cadastro/Cliente', component:RegisterClient},
-
-    //Footer
-    {path: '/Footer', component:footer},
-
-=======
     {path: '/cadastro/cliente', component:RegisterClient},
     {path: '/cadastro/atividade', component:RegisterActivity},
     {path: '/cadastro/agenda', component:RegisterAgenda},
@@ -45,7 +38,9 @@ const routes = [
     {path: '/cadastro/petcliente', component:RegisterPetClient},
     {path: '/cadastro/raça', component:RegisterRace},
     {path: '/cadastro/serviço', component:RegisterService},
->>>>>>> affe984aa42fc7d59ca42be2607100c78d3e7e23
+
+    //Footer
+    {path: '/footer', component:footer}
     
 ]
 const router = createRouter({

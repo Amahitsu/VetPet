961d1925a552bccdfe625bc16844dcfacd248b48
Amanda H Medeiros <71605324+Amahitsu@users.noreply.github.com>
2024-05-13 20:48:21 -0300

    Adição de rotas para listagem de cadastros

--- front/petfront/src/main.js
+++ front/petfront/src/main.js
@@ -17,35 +17,49 @@ import RegisterEmployee from './components/screensRegister/RegisterEmployee.vue'
 import RegisterPetClient from './components/screensRegister/RegisterPetClient.vue'
 import RegisterRace from './components/screensRegister/RegisterRace.vue'
 import RegisterService from './components/screensRegister/RegisterService.vue'
-
+import ListAnimal from './components/screenList/ListAnimal.vue'
+import ListClient from './components/screenList/ListClient.vue'
+import ListService from './components/screenList/ListService.vue'
+import ListEmployee from './components/screenList/ListEmployee.vue'
+import ListPetClient from './components/screenList/ListPetClient.vue'
+import ListRace from './components/screenList/ListRace.vue'
 
 
 const routes = [
     //pagina inicial options | login
-    {path: '/', component: Login},
-    {path: '/principal', component: PrincipalScreen},
+    { path: '/', component: Login },
+    { path: '/principal', component: PrincipalScreen },
 
     //rotas de agenda
-    {path: '/agenda/day', component:Day},
-    {path: '/agenda/month', component:Month},
-    {path: '/agenda/week', component:Week},
-    
+    { path: '/agenda/day', component: Day },
+    { path: '/agenda/month', component: Month },
+    { path: '/agenda/week', component: Week },
+
     //Registros de cadastro
-    {path: '/cadastro/cliente', component:RegisterClient},
-    {path: '/cadastro/atividade', component:RegisterActivity},
-    {path: '/cadastro/agenda', component:RegisterAgenda},
-    {path: '/cadastro/funcionario', component:RegisterEmployee},
-    {path: '/cadastro/petcliente', component:RegisterPetClient},
-    {path: '/cadastro/raça', component:RegisterRace},
-    {path: '/cadastro/serviço', component:RegisterService},
+    { path: '/cadastro/cliente', component: RegisterClient },
+    { path: '/cadastro/atividade', component: RegisterActivity },
+    { path: '/cadastro/agenda', component: RegisterAgenda },
+    { path: '/cadastro/funcionario', component: RegisterEmployee },
+    { path: '/cadastro/petcliente', component: RegisterPetClient },
+    { path: '/cadastro/raça', component: RegisterRace },
+    { path: '/cadastro/serviço', component: RegisterService },
 
     //Footer
-    {path: '/footer', component:footer}
-    
+    { path: '/footer', component: footer },
+
+    //Lista de cadastros
+    { path: '/list/animal', component: ListAnimal },
+    { path: '/list/cliente', component: ListClient },
+    { path: '/list/funcionario', component: ListEmployee },
+    { path: '/list/petclient', component: ListPetClient },
+    { path: 'list/Race', component: ListRace},
+    { path: '/list/servico', component: ListService },
+
+
 ]
 const router = createRouter({
     history: createWebHistory(),
-    routes, 
+    routes,
 })
 
 createApp(App).use(router).mount('#app')

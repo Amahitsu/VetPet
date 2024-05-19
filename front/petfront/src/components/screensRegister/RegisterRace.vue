ebc3bc1c20afed3d29e29b7e134a49cdfcd77fca
Gustavo Schwartz <gustavohschwartz@gmail.com>
2024-05-19 19:32:29 -0300

    Criando modais dos cadastros

--- front/petfront/src/components/screensRegister/RegisterRace.vue
+++ front/petfront/src/components/screensRegister/RegisterRace.vue
@@ -2,35 +2,36 @@
 </script>
 
 <template>
-    <section>
-        <form @submit.prevent="handleSubmit">
-            <h2>Cadastro de Raça</h2>
-
-            <div class="row mt-3">
-                <div class="col-md-6">
-                    <label for="inputName" class="form-label">Nome da Raça</label>
-                    <input type="text" class="form-control">
+    <div class="modal fade" id="modalRace" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Cadastro de Raça</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="col-md-6">
-                    <label for="inputZip" class="form-label">Espécie</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Selecione</option>
-                        <option value="1">Macho</option>
-                        <option value="2">Fêmea</option>
-                    </select>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="inputName" class="form-label">Nome da Raça</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputZip" class="form-label">Espécie</label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Selecione</option>
+                                <option value="1">Macho</option>
+                                <option value="2">Fêmea</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary me-2">Salvar</button>
+                    <button type="button" data-bs-dismiss="modal" class="btn btn-secondary">Cancelar</button>
                 </div>
             </div>
-
-            
-            <div class="row">
-                <div class="col-12 d-flex justify-content-end mt-4">
-                    <a class="btn btn-primary me-2">Salvar</a>
-                    <a @click="$router.go(-1)" class="btn btn-secondary">Cancelar</a>
-                </div>
-            </div>
-        </form>
-    </section>
+        </div>
+    </div>
 </template>
 
-<style>
-</style>
+<style></style>

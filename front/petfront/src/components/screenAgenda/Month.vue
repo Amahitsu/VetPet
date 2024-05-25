01720166d736cce0f3c30f4090cb8a2199839e64
Amanda H Medeiros <71605324+Amahitsu@users.noreply.github.com>
2024-05-24 21:45:15 -0300

    Amanda (#33)
    * adicionando agenda
    
    * Adicionando agenda

--- front/petfront/src/components/screenAgenda/Month.vue
+++ front/petfront/src/components/screenAgenda/Month.vue
@@ -1,8 +1,18 @@
 <script setup>
+import { useScreens } from 'vue-screen-utils';
+
+const { mapCurrent } = useScreens({
+    xs: '1000px',
+    sm: '640px',
+
+
+});
+const columns = mapCurrent({ lg: 2 }, 1);
+const expanded = mapCurrent({ lg: false }, true);
 </script>
 
 <template>
+    <VCalendar :columns="columns" :expanded="expanded" show-weeknumbers />
 </template>
 
-<style>
-</style>
+<style></style>

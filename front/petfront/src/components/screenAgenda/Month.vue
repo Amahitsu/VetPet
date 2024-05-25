ad75ff4f6025d13f02fef6936876aef37910a520
Amanda H Medeiros <71605324+Amahitsu@users.noreply.github.com>
2024-05-24 21:42:40 -0300

    Adicionando agenda

--- front/petfront/src/components/screenAgenda/Month.vue
+++ front/petfront/src/components/screenAgenda/Month.vue
@@ -1,25 +1,18 @@
 <script setup>
+import { useScreens } from 'vue-screen-utils';
+
+const { mapCurrent } = useScreens({
+    xs: '1000px',
+    sm: '640px',
+
+
+});
+const columns = mapCurrent({ lg: 2 }, 1);
+const expanded = mapCurrent({ lg: false }, true);
 </script>
 
 <template>
-    <div class="container calendar mt5">
-        <div class="row text-center header">
-            <div class="col">Dom</div>
-            <div class="col">Seg</div>
-            <div class="col">Ter</div>
-            <div class="col">Qua</div>
-            <div class="col">Qui</div>
-            <div class="col">Sex</div>
-            <div class="col">Sáb</div>
-        </div>
-    </div>
-    <div class="row text-center" id="calendar-body"></div>
-
+    <VCalendar :columns="columns" :expanded="expanded" show-weeknumbers />
 </template>
 
-<style>
-.calendar {
-    max-width: 600px;
-    margin: 0 auto;
-}
-</style>
+<style></style>
--- front/petfront/src/main.js
+++ front/petfront/src/main.js
@@ -4,12 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
-import Footer from './components/footer/footer.vue'
 import Login from './components/Login.vue'
 import PrincipalScreen from './components/PrincipalScreen.vue'
 import Day from './components/screenAgenda/Day.vue'
 import Month from './components/screenAgenda/Month.vue'
 import Week from './components/screenAgenda/Week.vue'
+import ListClient from './components/screenList/ListClient.vue'
+import ListEmployee from './components/screenList/ListEmployee.vue'
+import ListPetClient from './components/screenList/ListPetClient.vue'
+import ListRace from './components/screenList/ListRace.vue'
+import ListServiceType from './components/screenList/ListServiceType.vue'
+import ListSpecie from './components/screenList/ListSpecie.vue'
 import RegisterActivity from './components/screensRegister/RegisterActivity.vue'
 import RegisterAgenda from './components/screensRegister/RegisterAgenda.vue'
 import RegisterClient from './components/screensRegister/RegisterClient.vue'
@@ -19,18 +24,17 @@ import RegisterRace from './components/screensRegister/RegisterRace.vue'
 import RegisterService from './components/screensRegister/RegisterService.vue'
 import RegisterServiceType from './components/screensRegister/RegisterServiceType.vue'
 import RegisterSpecie from './components/screensRegister/RegisterSpecie.vue'
-import ListSpecie from './components/screenList/ListSpecie.vue'
-import ListClient from './components/screenList/ListClient.vue'
-import ListServiceType from './components/screenList/ListServiceType.vue'
-import ListEmployee from './components/screenList/ListEmployee.vue'
-import ListPetClient from './components/screenList/ListPetClient.vue'
-import ListRace from './components/screenList/ListRace.vue'
+
+import VCalendar from 'v-calendar'
+import 'v-calendar/style.css'
+
+
 
 
 const routes = [
     //pagina inicial options | login
     { path: '/', component: Login },
-    { 
+    {
         path: '/principal',
         component: PrincipalScreen,
         children: [
@@ -40,7 +44,7 @@ const routes = [
             { path: '/agenda/cadastro', component: RegisterAgenda },
             { path: '/petCliente/cadastro', component: RegisterPetClient },
             { path: '/raca/cadastro', component: RegisterRace },
-            { path: '/especie/cadastro', component: RegisterSpecie},
+            { path: '/especie/cadastro', component: RegisterSpecie },
             { path: '/atendimento/cadastro', component: RegisterService },
             { path: '/servico/cadastro', component: RegisterServiceType },
             { path: '/funcionario/cadastro', component: RegisterEmployee },
@@ -51,38 +55,41 @@ const routes = [
             { path: '/petsClientes', component: ListPetClient },
             { path: '/racas', component: ListRace },
             { path: '/servicos', component: ListServiceType },
-            { path: '/especies', component: ListSpecie},
+            { path: '/especies', component: ListSpecie },
 
 
-    //Registros de cadastro
-    { path: '/cadastro/cliente', component: RegisterClient },
-    { path: '/cadastro/atividade', component: RegisterActivity },
-    { path: '/cadastro/agenda', component: RegisterAgenda },
-    { path: '/cadastro/funcionario', component: RegisterEmployee },
-    { path: '/cadastro/petcliente', component: RegisterPetClient },
-    { path: '/cadastro/raça', component: RegisterRace },
-    { path: '/cadastro/servico', component: RegisterService },
-    { path: '/cadastro/especie', component: RegisterSpecie},
+            //Registros de cadastro
+            { path: '/cadastro/cliente', component: RegisterClient },
+            { path: '/cadastro/atividade', component: RegisterActivity },
+            { path: '/cadastro/agenda', component: RegisterAgenda },
+            { path: '/cadastro/funcionario', component: RegisterEmployee },
+            { path: '/cadastro/petcliente', component: RegisterPetClient },
+            { path: '/cadastro/raça', component: RegisterRace },
+            { path: '/cadastro/servico', component: RegisterService },
+            { path: '/cadastro/especie', component: RegisterSpecie },
 
 
-    //Lista de cadastros
-    { path: '/list/cliente', component: ListClient },
-    { path: '/list/funcionario', component: ListEmployee },
-    { path: '/list/petclient', component: ListPetClient },
-    { path: '/list/petclient', component: ListRace },
-    // { path: '/list/servico', component: ListService },
-    { path: '/list/specie', component: ListSpecie},
+            //Lista de cadastros
+            { path: '/list/cliente', component: ListClient },
+            { path: '/list/funcionario', component: ListEmployee },
+            { path: '/list/petclient', component: ListPetClient },
+            { path: '/list/petclient', component: ListRace },
+            // { path: '/list/servico', component: ListService },
+            { path: '/list/specie', component: ListSpecie },
             //rotas de agenda
             { path: '/agenda/day', component: Day },
-            { path: '/agenda/month', component: Month },
+            { path: '/agenda', component: Month },
             { path: '/agenda/week', component: Week }
         ]
-     },
-    
+    },
+
 ]
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router).use(VCalendar, {}).mount('#app')
+
+//createApp(App).use(router).mount('#app')

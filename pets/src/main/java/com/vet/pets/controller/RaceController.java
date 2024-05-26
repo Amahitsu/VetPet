688578709808f49ac782d950475ce5f4b0d5a7c1
Gustavo Schwartz <137568967+gustavohschwartz@users.noreply.github.com>
2024-05-26 00:02:50 -0300

    Alterações no cadastro/edição de raças (#34)
    * Alterando texto dos botões da tela de clientes
    
    * Alterando logo da navbar
    
    * Completando edição de raças

--- pets/src/main/java/com/vet/pets/controller/RaceController.java
+++ pets/src/main/java/com/vet/pets/controller/RaceController.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,7 +62,7 @@ public class RaceController {
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Deleted"));
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ApiResponse> updateBreed(@PathVariable Long id, @RequestBody RaceDTO dto) {
         Breeds breed = raceService.findBreed(id);
         if (breed == null) {

fd604298a611e18f3d8f581f3d8582154ce29961
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-06-07 20:10:41 -0300

    add register agenda html

--- front/petfront/src/components/screenAgenda/Month.vue
+++ front/petfront/src/components/screenAgenda/Month.vue
@@ -1,18 +1,29 @@
 <script setup>
 import { useScreens } from 'vue-screen-utils';
+import { useRouter } from 'vue-router';
 
 const { mapCurrent } = useScreens({
-    xs: '1000px',
-    sm: '640px',
-
-
+  xs: '1000px',
+  sm: '640px',
 });
+
 const columns = mapCurrent({ lg: 2 }, 1);
 const expanded = mapCurrent({ lg: false }, true);
+
+const router = useRouter();
+
+const goToAppointment = () => {
+  router.push('/agenda/cadastro');
+};
 </script>
 
 <template>
+  <div>
     <VCalendar :columns="columns" :expanded="expanded" show-weeknumbers />
+    <button @click="goToAppointment" class="btn btn-primary mt-3">Fazer Agendamento</button>
+  </div>
 </template>
 
-<style></style>
+<style>
+/* Adicione seus estilos aqui */
+</style>

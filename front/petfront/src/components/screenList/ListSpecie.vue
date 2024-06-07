69b84086d8d9ff3c0910bae64e88d760ef50c33a
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-06-06 21:08:26 -0300

    ajustes

--- front/petfront/src/components/screenList/ListSpecie.vue
+++ front/petfront/src/components/screenList/ListSpecie.vue
@@ -70,25 +70,6 @@
         </div>
     </div>
 
-    <div class="modal fade" id="deleteConfirmationModal" tabindex="-1" aria-labelledby="deleteConfirmationModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="deleteConfirmationModalLabel">Confirmar Exclusão</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Tem certeza que deseja excluir esta espécie?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-danger" @click="deleteSpecie(id)">Confirmar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
 </template>
 
 <script>

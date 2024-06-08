7971050493460d52d099fd5d09d9d12fe9443cdd
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-06-07 21:27:36 -0300

    Pedro (#53)
    * add layout confirm delete
    
    * add confirm delete race
    
    * save
    
    * arrumando hibernate
    
    * removendo bug
    
    * mini ajustes
    
    * ajustes
    
    * ajustes
    
    * tela delete specie
    
    * edit specie
    
    * ajustes
    
    * teste
    
    * ajustes
    
    * ajustes ListRa333
    
    * Pedro (#46) (#47)
    
    * add layout confirm delete
    
    * add confirm delete race
    
    * save
    
    * arrumando hibernate
    
    * removendo bug
    
    * mini ajustes
    
    * ajustes
    
    * ajustes
    
    * tela delete specie
    
    * edit specie
    
    * ajustes
    
    * teste
    
    * ajustes
    
    * ajustes ListRa333
    
    * ajuste listclient
    
    * ajustes
    
    * add register agenda html
    
    * agendamento
    
    * ajustes
    
    * funções em registerAgenda

--- pets/src/main/java/com/vet/pets/dto/AppointmentDTO.java
+++ pets/src/main/java/com/vet/pets/dto/AppointmentDTO.java
@@ -2,6 +2,6 @@ package com.vet.pets.dto;
 
 import java.sql.Date;
 
-public record AppointmentDTO(String start_time, String finish_time, Boolean medicines, Boolean vaccines, Date date, Long id_animals, Long id_workers, String observation) {
+public record AppointmentDTO(String start_time, String finish_time, Boolean medicines, Boolean vaccines, Date date, Long id_customer, Long id_animals, Long id_workers, Long id_services, String observation) {
     
 }

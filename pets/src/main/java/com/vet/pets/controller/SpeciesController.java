07707ebede3ea8f9570a770a09c0aff2854017b2
Gustavo Schwartz <137568967+gustavohschwartz@users.noreply.github.com>
2024-06-09 12:16:16 -0300

    Ajustando lista, cadastro e edição de especies (#55)

--- pets/src/main/java/com/vet/pets/controller/SpeciesController.java
+++ pets/src/main/java/com/vet/pets/controller/SpeciesController.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +61,7 @@ public class SpeciesController {
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Deleted"));
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ApiResponse> updateSpecie(@PathVariable Long id, @RequestBody SpecieDTO dto) {
         Species specie = speciesServices.findSpecie(id);
         if (specie == null) {

dcfd8fcac362c286ff220a141ef1f95d8f31fd2b
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-06-14 19:29:43 -0300

    ajuste worker

--- pets/src/main/java/com/vet/pets/controller/WorkerController.java
+++ pets/src/main/java/com/vet/pets/controller/WorkerController.java
@@ -1,26 +1,73 @@
 package com.vet.pets.controller;
+ 
+import java.util.List;
+import java.util.Optional;
 
-import com.vet.pets.dto.CustomerCreateDTO;
-import com.vet.pets.dto.WorkerCreateDTO;
-import com.vet.pets.service.SpeciesServices;
-import com.vet.pets.service.WorkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vet.pets.dto.WorkerCreateDTO;
+import com.vet.pets.entities.Worker;
+import com.vet.pets.service.WorkerServices;
+ 
 @RestController
 @RequestMapping("/api/v1/worker")
 public class WorkerController {
-
+ 
     @Autowired
     private WorkerServices workerServices;
-
+ 
     @PostMapping
-    public ResponseEntity<ApiResponse> createWorker(@RequestBody WorkerCreateDTO dto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(workerServices.createWorker(dto));
+    public ResponseEntity<ApiResponse> createWorker(@RequestBody WorkerCreateDTO dto) {
+        Optional<Worker> worker = workerServices.findWorkerByName(dto.name());
+        if (worker.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse("Funcionário já cadastrado."));
+        }
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<Worker>("Created", workerServices.createWorker(dto)));
+    }
+ 
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Worker>>> listWorkers() {
+        List<Worker> listWorkers = workerServices.listWorkers();
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<List<Worker>>("Ok", listWorkers));
+    }
+ 
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<Worker>> findWorker(@PathVariable Long id) {
+        Worker worker = workerServices.findWorker(id);
+        if (worker == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<Worker>("Ok", worker));
+    }
+ 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteWorker(@PathVariable Long id) {
+        Worker worker = workerServices.findWorker(id);
+        if (worker == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        workerServices.deleteWorker(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Deleted"));
+    }
+ 
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateSpecie(@PathVariable Long id, @RequestBody WorkerCreateDTO dto) {
+        Worker worker = workerServices.findWorker(id);
+        if (worker == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Espécie não encontrada."));
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<Worker>("Updated", workerServices.updateWorkerById(id, dto)));
     }
 }
--- pets/src/main/java/com/vet/pets/service/WorkerServices.java
+++ pets/src/main/java/com/vet/pets/service/WorkerServices.java
@@ -1,27 +1,87 @@
 package com.vet.pets.service;
+ 
+import java.util.List;
+import java.util.Optional;
 
-import com.vet.pets.controller.ApiResponse;
-import com.vet.pets.dto.WorkerCreateDTO;
-import com.vet.pets.entities.Worker;
-import com.vet.pets.repository.WorkerRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.vet.pets.dto.WorkerCreateDTO;
+import com.vet.pets.entities.Worker;
+import com.vet.pets.repository.WorkerRepository;
+
+import jakarta.transaction.Transactional;
+ 
 @Service
 public class WorkerServices {
     @Autowired
     private WorkerRepository workerRepository;
-
+ 
     @Autowired
     private PasswordEncoder passwordEncoder;
+ 
     @Transactional
-    public ApiResponse createWorker(WorkerCreateDTO dto){
+    public Worker createWorker(WorkerCreateDTO dto){
         try{
-            workerRepository.save(new Worker(null, dto.name(), dto.cpf(), dto.phone(),
-                    dto.address(), dto.function(), dto.username(), passwordEncoder.encode(dto.password()), dto.userLevel(), dto.active()));
-            return new ApiResponse("Created!");
+            Worker newWorker = workerRepository.save(new Worker(null, dto.name(), dto.cpf(), dto.phone(),
+            dto.address(), dto.function(), dto.username(), passwordEncoder.encode(dto.password()), dto.userLevel(), dto.active()));
+            return newWorker;
+        } catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+ 
+    public List<Worker> listWorkers(){
+        try{
+            List<Worker> Worker = workerRepository.findAll();
+            return Worker;
+        } catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+ 
+    public Worker findWorker(Long id){
+        try{
+            Optional<Worker> worker = workerRepository.findById(id);
+            if(worker.isEmpty()) {
+                return null;
+            }
+ 
+            return worker.get();
+ 
+        } catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+ 
+    public  Optional<Worker> findWorkerByName(String name){
+        try{
+            Optional<Worker> Worker = workerRepository.findByName(name);
+            return Worker;
+ 
+        } catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+ 
+    public void deleteWorker(Long id){
+        try{
+            workerRepository.deleteById(id);
+         } catch (Exception e){
+             throw new RuntimeException(e.getMessage());
+         }
+    }
+ 
+    public Worker updateWorkerById(Long id, WorkerCreateDTO dto){
+        try{
+            Optional<Worker> worker = workerRepository.findById(id);
+            if(worker.isEmpty()) {
+                return null;
+            }
+            Worker newWorker = workerRepository.save(new Worker(id, dto.name(), dto.cpf(), dto.phone(),
+            dto.address(), dto.function(), dto.username(), passwordEncoder.encode(dto.password()), dto.userLevel(), dto.active()));
+            return newWorker;
         } catch (Exception e){
             throw new RuntimeException(e.getMessage());
         }
